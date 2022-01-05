981b17eafa7ebc66788faced78d2bfea8ae5bef1
ChristopherEverett <ChristopherEverettMiller@gmail.com>
2022-01-05 11:33:51 -0800

    Implemented Dataclasses, reorganized code, resolved a bug with placing worms in invalid coordinates.

--- duksu.py
+++ duksu.py
@@ -2,6 +2,7 @@
 import curses
 import random
 from curses import wrapper
+from dataclasses import dataclass
 
 screen = curses.initscr()
 maxY, maxX = screen.getmaxyx()
@@ -10,54 +11,43 @@ curses.curs_set(0)
 WORM_OBJS = []
 
 
-class Win:
-    pass
+@dataclass
+class Animal:
+    posY: int
+    posX: int
+    icon: str
 
 
-class Worm:
+@dataclass
+class Worm(Animal):
     """Worms that Duksu can eat."""
-
-    def __init__(self, posY, posX, icon="~"):
-        self.posY = posY
-        self.posX = posX
-        self.position = [posY, posX]
-        self.icon = icon
 
     # Return Worm's position and '~'
     def setWorm(self):
         return self.posY, self.posX, self.icon
 
 
-class Pet:
+@dataclass
+class Duck(Animal):
     """Pet duck that wanders around the screen and pecks at worms.
     Future features include 'talking' with owner, splashing in a pond,
     having a 'hunger meter' and needing to be fed by owner."""
 
     DIRECTIONS = ["RIGHT", "LEFT", "UP", "DOWN"]
-
-    def __init__(self, name, posY=1, posX=1):
-        self.name = name
-        self.direction = "RIGHT"
-        self.position = [posY, posX]
-        self.posY = posY
-        self.posX = posX
-        self.icon = "D"
-        self.wormsEaten = 0
+    direction = "RIGHT"
+    wormsEaten = 0
+    hunger = 0
+    fun = 100
 
     # Increments wormsEaten, removes worm from screen and from WORM_OBJS
     def peck(self, wormObject, winObject):
         self.wormsEaten += 1
-        winObject.delch(wormObject.posY, wormObject.posX)
+        winObject.addstr(wormObject.posY, wormObject.posX, ' ')
         WORM_OBJS.remove(wormObject)
 
     # Change which direction duck is facing.
     def facing(self):
         self.direction = random.choice(self.DIRECTIONS)
-
-        if self.direction == "RIGHT" or self.direction == "UP":
-            self.icon = "D"
-        else:
-            self.icon = "ᗡ"
 
     # Move around screen depending on direction faced, making sure it is within windows boundary.
     def waddle(self, winObject):
@@ -115,8 +105,8 @@ class Windows:
 
 # Create worm objects and store it in WORM_OBJ list. Try to put this in Worm class.
 def makeWorm(winObject):
-    randY, randX = random.randrange(1, winObject.height - 2), random.randrange(1, winObject.width - 2)
-    WORM_OBJS.append(Worm(randY, randX))
+    randY, randX = random.randrange(2, winObject.height - 2), random.randrange(2, winObject.width - 2)
+    WORM_OBJS.append(Worm(randY, randX, '~'))
 
 
 def modeInput(winObject, mode):
@@ -135,63 +125,66 @@ def modeInput(winObject, mode):
     elif ch == -1:
         return mode
 
+
 # Main function with curses screen passed to it for Wrapper()
 def main(screen):
-    duck = Pet('Ducksu')
+    duck = Duck(1, 1, 'D')
     mode = 'RUN'
+
+    # Initialize Curses windows
+    top_left = Windows('', int(maxY), int(maxX - (maxX / 3)), 0, 0)
+    right = Windows('', int(maxY), int(maxX / 3), 0,
+                    int(maxX - (maxX / 3)))
+
+    # List Duck stats in right window
+    right.addstr(0, right.width // 2, 'Duksu')
+
     while mode == 'RUN':
-        # Initialize Curses windows
-        Win.top_left = Windows('', int(maxY), int(maxX - (maxX / 3)), 0, 0)
-        Win.right = Windows('', int(maxY), int(maxX / 3), 0,
-                               int(maxX - (maxX / 3)))
 
-        # List Duck stats in right window
-        Win.right.addstr(0, Win.right.width//2, duck.name)
-
-        mode = modeInput(Win.top_left, mode)
+        mode = modeInput(top_left, mode)
 
         while mode == 'MENU':
-            mode = modeInput(Win.top_left, mode)
-            Win.right = Windows('Worms Eaten: ' + mode, int(maxY), int(maxX / 3), 0, int(maxX - (maxX / 3)))
-            Win.top_left.addstr(duck.placeIcon()[0], duck.placeIcon()[1], duck.placeIcon()[2])
+            mode = modeInput(top_left, mode)
+            right = Windows('Worms Eaten: ' + mode, int(maxY), int(maxX / 3), 0, int(maxX - (maxX / 3)))
+            top_left.addstr(*duck.placeIcon())
 
             # Put code here to add menu options
 
             # Refresh all windows
-            Win.top_left.refresh()
-            Win.right.refresh()
+            top_left.refresh()
+            right.refresh()
 
         # Place Duck in Top Left window
-        Win.top_left.addstr(duck.placeIcon()[0], duck.placeIcon()[1], duck.placeIcon()[2])
+        top_left.addstr(*duck.placeIcon())
 
         # Refresh all windows
-        Win.top_left.refresh()
-        Win.right.refresh()
+        top_left.refresh()
+        right.refresh()
 
         # ~2% chance to spawn a worm if less than 5 worms are present every 'tick'
         if random.randrange(0, 1000) <= 20 and len(WORM_OBJS) < 5:
-            makeWorm(Win.top_left)  # Worm objects are stored in the global list WORM_OBJS
+            makeWorm(top_left)  # Worm objects are stored in the global list WORM_OBJS
 
         # Iterate through worm objects
         for i in WORM_OBJS:
             # Check if duck and worm are on the same square
             if duck.posY == i.posY and duck.posX == i.posX:
                 # peck() increments 'wormsEaten', deletes '~' from position, removes worm object from WORM_OBJ
-                duck.peck(i, Win.top_left)
-                Win.top_left.refresh()
+                duck.peck(i, top_left)
+                top_left.refresh()
                 break
 
             # Draw '~' at random y,x within top left window, then update window.
-            Win.top_left.addstr(i.setWorm()[0], i.setWorm()[1], i.setWorm()[2])
-            Win.top_left.refresh()
+            top_left.addstr(*i.setWorm())
+            top_left.refresh()
 
         # 1 'tick' = 750 ms
-        curses.napms(750)
+        curses.napms(50)
 
         # Set direction duck is facing, change icon, move 1 square in that direction
+        top_left.addstr(duck.posY, duck.posX, ' ')
         duck.facing()
-        duck.waddle(Win.top_left)
-
+        duck.waddle(top_left)
 
 
 # Call main through curses.wrapper
